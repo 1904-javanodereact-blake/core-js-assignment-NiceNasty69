f92cd465c4bd89ed69f1511a62fd4b57ce2bfd9a
NiceNasty <whildkhild@yahoo.com>
2019-04-02 23:58:28 -0400

    Fibonacci complete

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,20 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
 
+
+function fib(n) {
+let temp = 1; 
+let prev1 = 1;
+let result;
+ for (i = 3; i < n + 1; i++) {
+    result  = temp  + prev1;
+    prev1 = temp;
+    temp = result;
+ }
+ if (n < 3) { 
+     return 1;
+ }
+ return result;
 }
+console.log(fib(5));

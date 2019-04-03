25cc2ed02f097adda6c2e99e21696cc5f5f51b08
NiceNasty <whildkhild@yahoo.com>
2019-04-03 00:33:01 -0400

    Created Object'

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,18 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let serialKiller = {
+    name: 'Jason Voorhees',
+    age: 'Unknown', //He's died and come back so many times who really knows
+    weapon: 'Butchers Knife',
+    favKill: 'Anybody that is unlucky enough to cross his path'
+
+}
+console.log(serialKiller);
+console.log(`serial killer name = ${serialKiller.name}`);
+console.log(`serial killer name = ${serialKiller['name']}`);
+
+serialKiller.weakness = 'Innocents'; // time and time again Jason has had opportunities to harm children yet he did not
+delete serialKiller.favKill;
+console.log(serialKiller);

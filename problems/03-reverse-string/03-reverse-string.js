ec23f9d6905e50f51fda1fa7ed5ba2ce89408324
NiceNasty <whildkhild@yahoo.com>
2019-04-04 00:21:37 -0400

    Completed strring reversal

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,22 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
+
+
 function reverseStr(someStr) {
-  
+    x = someStr.length;
+    n = ""
+    for(let a = x - 1; a >= 0 ; a--) {
+        n = n + someStr[a];   
+    }
+    
+    return n;
 }
+
+
+console.log(reverseStr("Have"));
+console.log(reverseStr("Some"));
+console.log(reverseStr("Tea"));
+console.log(reverseStr("With"));
+console.log(reverseStr("Your"));
+console.log(reverseStr("Friend"));

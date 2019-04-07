ee15772617267744a17aea472ed8eb073c3724ed
NiceNasty <whildkhild@yahoo.com>
2019-04-07 13:25:14 -0400

    Palindrome first round

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,17 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    let niceStr = someStr
+    let tatStr = ((niceStr.split('')).reverse()).join('');
+    if (niceStr !== tatStr) {
+        return false;
+    } else {
+        return true;
+    }
 }
+
+console.log(isPalindrome('evitative'));
+console.log(isPalindrome('sea'));
+console.log(isPalindrome('radar'));
+console.log(isPalindrome('nunchuck'));
+console.log(isPalindrome('kayak'));

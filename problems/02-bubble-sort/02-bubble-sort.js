5336da1346107765df2645c2c27c34b5e1f6e80b
NiceNasty <whildkhild@yahoo.com>
2019-04-06 23:14:27 -0400

    Finally Factored

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,12 +3,18 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-
+    //First For loop iteration to reach first elment jagArr n
         for(let i = 0; i < jagArr.length - 1; i++){
+    //Second For loop iteration to reach second element jagArr n+1
             for(let n = 0; n < jagArr.length - 1; n++){
+    //If statment to compare the elements
                 if (jagArr[n] > jagArr[n+1]){
+    //var temp is assigned the jagArr n position
                     let temp = jagArr[n]
+    //jagArr is assigned the jagArr n+1 position
                     jagArr[n] = jagArr[n+1]
+    //jagArr n+1 is assigned the temp position
+    //var temp is an empty place holder
                     jagArr[n+1]= temp;
                 }
             }
@@ -17,8 +23,10 @@ function bubbleSort(numArray) {
       
     }
     
-
+    //jagArr is the array being cycled through
     jagArr = [17,21,3,56,75,99,801];
+    //function call
     bubbleSort(jagArr);
+    //format for printing string
     console.log(`${jagArr}`);
     
--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,17 +2,13 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-//if statement to find out if the sumNum is equal to 0
     if (sumNum === 0) {
-//if so return 1
+
         return 1;
-//otherwise return the equivalent value of variable times 
-// the fucntion name times the variable minus itself
+
     } else {
         return sumNum * factorial(sumNum - 1);
     }
 }
-//print to screen by way of string interpolation
-// back tics with the ${} string interpolation allows 
-//JS expressions directly into the string
+
 console.log(`Your factorial is ${factorial(2)}`)

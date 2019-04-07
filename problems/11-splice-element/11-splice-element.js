b43df45c15ccefd50b4542ad88c6839350c3b248
NiceNasty <whildkhild@yahoo.com>
2019-04-07 15:45:25 -0400

    Splice your brain matter

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,17 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    let fifArr = [];
+    for (i = 0; i < index; i++) {
+        fifArr.push(someArr[i]);
+    }
+    let tifArr = [];
+    for (i = 3; i < someArr.length - 1; i++) {
+        tifArr.push(someArr[i]);
+    }
+    return [fifArr, tifArr];
 }
+let dimArr = [7, true, 25, 'Nancy', -5, 'Ocean'];
+let fip = 3
+let result = spliceElement(dimArr, fip);
+console.log(result)
